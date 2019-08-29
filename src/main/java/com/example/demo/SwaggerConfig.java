975d5805471bf597559b98201c538c046599bef6
Sindhu2310 <bhattaramsindhura603@gmail.com>
2019-08-29 11:30:13 +0000

    Added Metadata for Swagger

--- src/main/java/com/example/demo/SwaggerConfig.java
+++ src/main/java/com/example/demo/SwaggerConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,5 +19,14 @@ public class SwaggerConfig{
     public Docket employeeAPI(){
         return new Docket(DocumentationType.SWAGGER_2).
         select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
+    }
+
+    public ApiInfo metaData(){
+         return new ApiInfo("Learning on Integration of Spring Data Rest+ React js",
+         "This project contains my learnings on Spring Data Rest+React JS",
+         "1.0",
+         "Terms Of Service",
+        new Contact("Sindhura", "", ""),null,null);
+
     }
 }
